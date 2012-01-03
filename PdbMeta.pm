efc13e4f7903b05480574db3ccb8c697880de7c3
Thomas Juettemann <juettemann@gmail.com>
2012-01-03 19:01:42 +0100

    New hope

---
+++ PdbMeta.pm
@@ -0,0 +1,186 @@
+package PdbMeta;
+
+use 5.10.0;
+use strict;
+use warnings;
+
+use Digest::MD5::File qw(file_md5);
+use Log::Log4perl qw(get_logger);
+use Data::Dumper;
+use Carp;
+
+use feature qw(switch say);
+
+use constant UNIT  => {
+  MONOMERIC      => 1, 
+  DIMERIC        => 2, 
+  TRIMERIC       => 3, 
+  TETRAMERIC     => 4, 
+  PENTAMERIC     => 5, 
+  HEXAMERIC      => 6, 
+  HEPTAMERIC     => 7, 
+  OCTAMERIC      => 8, 
+  NONAMERIC      => 9, 
+  DECAMERIC      => 10, 
+  UNDECAMERIC    => 11, 
+  DODECAMERIC    => 12, 
+  TRIDECAMERIC   => 13, 
+  TETRADECAMERIC => 14, 
+  PENTADECAMERIC => 15, 
+  HEXADECAMERIC  => 16, 
+  HEPTADECAMERIC => 17, 
+  OCTADECAMERIC  => 18, 
+  NONADECAMERIC  => 19, 
+  EICOSAMERIC    => 20 
+};
+
+sub parseRemark350 {
+  my ($self, $file) = @_;
+  
+  confess "File location is missing" if(!$file); 
+  confess "'$file' does not exists/not readable" if(! -T $file); 
+  
+  my ($id, $reading) = (0,0);
+  my $result;
+
+  open(my $fh,'<', $file) or die "Can not open/access '$file'\n$!";
+    while(my $line = <$fh>){
+      next unless ($line =~ /^REMARK/);
+      
+      if (!$reading && $line =~ /^REMARK 350 BIOMOLECULE: (\d+)/ ){
+        $reading = 1;
+        $id = $1;
+      }
+      elsif($reading && 
+          $line =~ /AUTHOR DETERMINED BIOLOGICAL UNIT: (.*)/) {
+        confess $line if(!$1);
+        $result->{$id}->{author} = _getUnitValue($1);
+
+      }
+      elsif($reading && 
+          $line =~ /SOFTWARE DETERMINED QUATERNARY STRUCTURE: (.*)/) {
+        confess $line if(!$1);
+        $result->{$id}->{software} = _getUnitValue($1);
+      }
+      elsif($reading && 
+          $line =~ /SOFTWARE USED: (\w+)/) {
+        $result->{$id}->{method} = $1;
+      }
+      elsif($reading && 
+          $line =~ /TOTAL BURIED SURFACE AREA: (\d+)/) {
+        $result->{$id}->{buried} = $1;
+      }
+      elsif($reading && 
+          $line =~ /SURFACE AREA OF THE COMPLEX: (\w+)/) {
+        $result->{$id}->{surface} = $1;
+      }
+      elsif($reading && 
+          $line =~ /CHANGE IN SOLVENT FREE ENERGY: (\S+) /) {
+        $result->{$id}->{energy} = $1;
+      }
+      elsif($reading && 
+          $line =~ /^REMARK 350\s+$/){
+        $reading = 0;
+      }
+      else {
+        $line =~ /REMARK\s*(\d{1,4})/;
+        my $remark = $1;
+        last if($remark > 350);
+      }
+
+    }
+  close($fh);
+  return($result);
+}
+###############################################################################
+#
+###############################################################################
+sub _getUnitValue {
+  my ($unit) = @_;
+  confess "Unit missing" if(!$unit);
+  $unit =~ s/\s//g;
+
+  my $value;
+  if($unit =~ /^(\d\d+)-MERIC$/){
+    $value = $1;
+  }
+  elsif($unit =~ /^(\w+)$/) {
+    $value = UNIT->{$1};
+    die "'$value' not defined in lookup" if(!$value);
+  }
+  else{die "Something odd with $unit"}
+  return($value);
+}
+###############################################################################
+#                           getNumberOfBiomolecules
+###############################################################################
+# REMARK 300 BIOMOLECULE: 1, 2
+sub getNumberOfBiomolecules {
+  my ($self, $file) = @_;
+  confess "File location is missing" if(!$file); 
+  confess "'$file' does not exists/not readable" if(! -T $file); 
+
+  my @molecules;
+  my $found = 0;
+
+  open(my $fh,'<', $file) or die "Can not open/access '$file'\n$!";
+  while(my $line = <$fh>){
+    next unless ($line =~ /^REMARK 300 BIOMOLECULE:(.*)/);
+    $found = 1;
+    my $amount = $1;
+    $amount =~ s/\s//g;
+# make sure the numbers are consecutive
+    if($amount =~ /,/){
+      @molecules = split(/,/, $amount);
+    }
+    elsif(length($amount) == 1) {
+      push(@molecules, $amount);
+    }
+    else{
+      warn "Something wrong, line:\n$line\n";
+    }
+    last;
+  }
+  close($fh);
+
+# No remark Biomolecule 300 exist, create it manuallt by parsing remark 350
+  if(!$found){
+  open(my $fh,'<', $file) or die "Can not open/access '$file'\n$!";
+    while(my $line = <$fh>){
+      next unless ($line =~ /^REMARK/);
+      if ($line =~ /^REMARK 350 BIOMOLECULE:(.*)/){
+        my $amount = $1;
+        $amount =~ s/\s//g;
+        die if($amount !~ /^[0-9]*$/);
+        push(@molecules, $amount);
+      }
+      else{
+        $line =~ /REMARK\s*(\d{1,4})/;
+        my $remark = $1;
+        last if($remark > 350);
+      }
+    }
+  close($fh);
+    
+  }
+  return(\@molecules);
+}
+
+
+
+###############################################################################
+#                                       New                                   #
+###############################################################################
+# Constructor
+###############################################################################
+sub new {
+  my ($class) = @_;
+
+  my $self = {};
+
+  bless($self, $class);
+  return($self);
+}
+###############################################################################
+
+1;
---
+++ main.pl
@@ -0,0 +1,215 @@
+#!/usr/bin/perl
+
+use 5.10.0;
+use strict;
+use warnings;
+
+use Digest::MD5::File qw(file_md5);
+use Log::Log4perl qw(get_logger);
+use Data::Dumper;
+use XML::LibXML;
+use Carp;
+
+use PdbMeta;
+
+use feature qw(switch say);
+
+my $file = 'pdb1fin.ent';
+my $meta  = PdbMeta->new();
+
+
+&main();
+
+sub main(){
+  my $cfg = XML::LibXML->new();
+#  my $entDir = $cfg->findvalue('/cfg/pdb_ent_dir');
+
+#  my $xray = identifyCrystalStructures($cfg->findvalue('/cfg/pdbaa'));
+  my $xray = identifyCrystalStructures('./pdbaa');
+
+  foreach my $pdbID (sort keys %{$xray}){
+    my $entFile = buildPdbFilePath($pdbID, 'ent');
+    my $amountStructures = $meta->getNumberOfBiomolecules($entFile);
+    confess "Error\n$entFile: $amountStructures" if(!$amountStructures);
+    next if(scalar(@{$amountStructures}) == 1);
+    my $remark350 = $meta->parseRemark350($entFile);
+
+
+# check if assemblies with different amount of chains exist.
+# get the one with more chains if
+# if several with the same amount of chains exists, use the one with lowest energy
+# 
+# read chain section, check if chain IDs have been used multiple times
+# if so, rename keep relation
+# split, identify interacting files
+# homo/hetero distinction
+# cluster
+
+#print Dumper($amountStructures); 
+#print Dumper($remark350); 
+  }
+
+
+}
+###############################################################################
+#                                   _findLargestAssembly
+###############################################################################
+# Identify the largest assembly with the lowest free energy in PDB ent files
+# In many cases, the file contains this remark:
+# REMARK 300 BIOMOLECULE: 1, 2
+# The numbers seem to concure to the PDB files available in the biounit sub-
+# directory. Parsing REMARK 350, that should be available in all files,
+# the assembly/structure with the most chains/lowest free energy is identified
+# It is also checked if author and software agree on the biounit
+
+###############################################################################
+sub _findLargestAssembly {
+  my ($remark350) = @_;
+  my $last;
+  $last->{chains} = 0;
+  $last->{energy} = 1000;
+  $last->{biounit} = 0;
+# Check if both author and software determined values exists for molecules
+# Compare if they are equal
+# Compare if they are a) more chains than former molecule b) delta G is lower
+  foreach my $unit (sort keys %{$remark350}){
+
+    if( (exists $remark350->{$unit}->{author}) && 
+        (exists $remark350->{$unit}->{software}) ){
+
+      if($remark350->{$unit}->{author} != $remark350->{$unit}->{software}){
+        die "Different, check it" 
+      }
+
+      if($remark350->{$unit}->{software} > $last->{chains}){
+        $last->{chains}  = $remark350->{$unit}->{software};
+        $last->{energy}  = $remark350->{$unit}->{energy};
+        $last->{biounit} = $remark350->{$unit}->{biounit};
+      }
+      if($remark350->{$unit}->{software} = $last->{chains}){
+        if(defined $remark350->{$unit}->{energy} and 
+            $remark350->{$unit}->{energy} < $last->{energy}){
+
+          $last->{energy}  = $remark350->{$unit}->{energy};
+          $last->{biounit} = $remark350->{$unit}->{biounit};
+        }
+      }
+      next;
+    }
+# If only software exists, only replace if more chains or equal chains and lower gibbs
+# ToDo: PISA/PQS
+    if(exists $remark350->{$unit}->{software}){
+      if($remark350->{$unit}->{software} > $last->{chains}){
+        $last->{chains} = $remark350->{$unit}->{software};
+        $last->{energy}   = $remark350->{$unit}->{energy};
+        $last->{biounit}  = $remark350->{$unit}->{biounit};
+      }
+      if($remark350->{$unit}->{software} =  $last->{chains}){
+        if(defined $remark350->{$unit}->{energy} and 
+            $remark350->{$unit}->{energy} < $last->{energy}){
+          $last->{chains} = $remark350->{$unit}->{software};
+          $last->{energy}   = $remark350->{$unit}->{energy};
+          $last->{biounit}  = $remark350->{$unit}->{biounit};
+        }
+      }
+      next;
+    }
+#
+    if(exists $remark350->{$unit}->{author}){
+      if($remark350->{$unit}->{author} > $last->{chains}){
+        $last->{chains}   = $remark350->{$unit}->{author};
+        $last->{energy}   = $remark350->{$unit}->{energy};
+        $last->{biounit}  = $remark350->{$unit}->{biounit};
+      }
+      next;
+    }
+  }
+}
+
+# 1fnt-> fn/1nft
+sub buildPdbFilePath {
+  my ($pdbID,$type) = @_;
+  $type = 'normal' if (!$type);
+
+  my $subDir = substr($pdbID,1,2);
+  my $path;
+  $path = "$subDir/pdb$pdbID.ent" if ($type eq 'ent');
+  $path = "$subDir/$pdbID.pdb"    if ($type eq 'normal');
+  return($path);
+
+}
+# read pdbaa, keep crystal structures
+#>200LA 164 XRAY
+#PDB+chain ID given, return only PDB ID in lowercase
+sub identifyCrystalStructures {
+  my ($file) = @_;
+  say $file;
+  my $result;
+  open(my $fh,'<', $file) or die "Can not open/access '$file'\n$!";
+    while(my $line = <$fh>){
+      next unless($line =~ />(\w{5})\s\d*\sXRAY/);
+      my $pdbID = substr($1,0,4);
+      $pdbID = lc($pdbID);
+      $result->{$pdbID}++;
+    }
+  close($fh);
+return($result);
+
+
+}
+=head1 NAME
+
+script.pl
+
+
+=head1 SYNOPSIS
+
+script.pl
+
+=head1 AUTHOR
+
+Thomas Juettemann <juettemann@gmail.com>
+
+
+=head1 ARGUMENTS
+
+=over 8
+
+=item none
+
+=back
+
+
+=head1 OPTIONS
+
+=over 8
+
+=item none
+
+=back
+
+=head1 DESCRIPTION
+
+B<script.pl> 
+
+=head1 BUGS
+
+=over 4
+
+=item None known
+ 
+=back
+
+=head1 TODO
+
+=over 4
+
+=item Nothing
+
+=back
+
+=head1 UPDATES
+
+=cut
+
+
