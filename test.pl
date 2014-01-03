b06f3b3ff83426a78754c826b1a49bf884d2f0d0
Thomas Juettemann <juettemann@gmail.com>
2014-01-03 15:00:15 +0000

    test

---
+++ test.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+=head1 LICENSE
+
+  Copyright (c) 1999-2013 The European Bioinformatics Institute and
+  Genome Research Limited.  All rights reserved.
+
+  This software is distributed under a modified Apache license.
+  For license details, please see
+
+    http://www.ensembl.org/info/about/code_licence.html
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <ensembl-dev@ebi.ac.uk>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>.
+
+=head1 NAME
+
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use Bio::EnsEMBL::Utils::Argument  qw(rearrange );
+use Bio::EnsEMBL::Utils::Exception qw(throw warning deprecate);
+use Bio::EnsEMBL::Utils::EFGUtils  qw(dump_data);
+
+main();
+
+sub main {
+
+}
+
+
